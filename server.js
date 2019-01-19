003430340d5d42860e08fe5a2cca519b9c2653a5
unknown <kd00441@gmail.com>
2019-01-19 17:18:22 -0500

    homework

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const http=require('http');
-const hostname="myInto";
+const hostname="127.0.0.1";
 const port=3000;
 
 //create server on the parameter
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello from Komaldeep Kaur.My Student Id is 300926834\n');
+    res.end('Hello, My name is Komaldeep Kaur.\nMy Student Id is 300926834\nI am pursuing Software Engineering Technology');
   });
   server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
